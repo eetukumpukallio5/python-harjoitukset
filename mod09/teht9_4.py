9099febacfe18df1dc0e1251e12a7b6c779d0aee
Eetu Kumpukallio <eetukumpukallio5@gmail.com>
2026-09-17 01:11:07 +0300

    9_4 edistystä melkein valmis

--- mod09/teht9_4.py
+++ mod09/teht9_4.py
@@ -9,9 +9,12 @@
 #Kilpailu jatkuu, kunnes jokin autoista on edennyt vähintään 10000 kilometriä. 
 #Lopuksi tulostetaan kunkin auton kaikki ominaisuudet selkeäksi taulukoksi muotoiltuna.
 
+
 #käytetään ohjelmassa tehtävän 9_3 auto luokkaa. arvotaan for loopilla kymmenelle autolle satunnainen huippunopeus 100-200 km/h.
-#tallennetaan autojen tiedot olioihin 1-10, ja lisätään ne kaikki sanakirjaan. 
-#siirrytään silmukkaan, jossa kaikki autot kiihtyvät satunnaisen määrän -10 - 15 välillä. toistetaan silmukkaa kunnes yksi autoista saavuttaa 10000 km.
+#tallennetaan autojen tiedot listan paikkoihin 1-10, joita voidaan verrata tarvittaessa
+#siirrytään silmukkaan, jossa kaikki autot kiihtyvät satunnaisen määrän -10 - 15 välillä. siirrytään sitten 1 tunnin verran.
+#tulostetaan 3 tunnin välein johtaja ja jäljellä oleva matka, lisää hieman vuorovaikutteisuutta
+#toistetaan silmukkaa kunnes yksi autoista saavuttaa 10000 km.
 #tulostetaan autot kuljettujen matkojen mukaan.
 
 import random
@@ -33,11 +36,27 @@ class Auto:
     def kulje(self, aika):
         self.kuljettu_matka += (aika * self.tamanhetkinen_nopeus)
 
-autot = []
 
-for i in range(1, 11):
-    "auto{i}" = Auto(f"ABC-{i}", random.randint(100, 200))
 
-print(autot)
-    
-    
+autot = ["auto1", "auto2", "auto3", "auto4", "auto5", "auto6", "auto7", "auto8", "auto9", "auto10"]
+tunti = 0
+karkimatka = 0
+
+
+for i in range(10):
+    autot[i] = Auto(f"ABC-{i + 1}", random.randint(100, 200))
+
+print("Kilpa-ajot alkakoot!")
+
+while karkimatka < 10000:
+    karkimatka = 0
+    for i in range(10):
+        autot[i].kiihdyta(random.randint(-10, 15))
+        autot[i].kulje(1)
+        if autot[i].kuljettu_matka >= karkimatka:
+            karkimatka = autot[i].kuljettu_matka
+            pisin = autot[i]
+    tunti += 1
+    if tunti % 3 == 0:
+        print(f"Tunteja on kulunut {tunti}! Kärjessä on auto {(pisin).rekisteritunnus}. Vielä {10000 - karkimatka} kilometria maaliin!")
+        input("paina enter jatkaaksesi...")
